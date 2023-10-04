05b4668c5bcd8a32485cdd35a233a78195e0f503
Lukas <lukas_flores.8@hotmail.com>
2023-10-04 20:55:46 -0300

    development environment

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { authProvider } from "./authProvider";
+
+export const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource
+      name="users"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+    <Resource
+      name="papers"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+    <Resource
+      name="news"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+  </Admin>
+);
